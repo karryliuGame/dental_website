ece7c8d44d7c8142fbe90cd3e1e348c59ea9cc12
Karry Liu <89614779+karryliuGame@users.noreply.github.com>
2021-11-09 16:28:08 -0800

    typo fix
    typo of whitenoise

--- dental/settings.py
+++ dental/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenose.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dental.urls'
